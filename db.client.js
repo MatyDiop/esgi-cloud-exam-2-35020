6de70f63e0ac88ab3fc700fba3de1adcfa755b74
matydiop <maty0012.dp@gmail.com>
2025-03-04 11:25:15 +0100

    Utilisation simplifiée de DATABASE_URL

--- db.client.js
+++ db.client.js
@@ -1,24 +1,16 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config(); // Charge les variables d'environnement
 
-// Connexion à PostgreSQL avec les variables d'environnement
-const sequelize = new Sequelize(
-  process.env.DB_NAME, // Nom de la base de données
-  process.env.DB_USER, // Nom d'utilisateur PostgreSQL
-  process.env.DB_PASSWORD, // Mot de passe PostgreSQL
-  {
-    host: process.env.DB_HOST, // Hôte PostgreSQL
-    port: process.env.DB_PORT || 5432, // Port par défaut PostgreSQL
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
+const databaseUrl = process.env.DATABASE_URL;
+
+const sequelize = new Sequelize(databaseUrl, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-    logging: false, // Désactive les logs SQL
-  }
-);
+  },
+});
 
 // Vérification de la connexion
 sequelize.authenticate()
